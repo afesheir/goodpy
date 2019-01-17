691f2c9fdf391e7139fb9053d767ab52e5190cd4
U-SYNOPSYS\shokry <shokry@shokry-e7270.internal.synopsys.com>
2019-01-17 18:52:18 +0200

    Fixed some stuff

--- math.py
+++ math.py
@@ -1,10 +1,13 @@
 import math
 
+# This function adds two numbers
 def add(x,y):
 	return x + y
 
+# This function subs two numbers
 def sub(x,y):
 	return x - y
 
+# This function muls two numbers
 def mul(x,y):
-	return x * y
+	return (x) * (y)
